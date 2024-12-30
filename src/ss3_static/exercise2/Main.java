e4d56aedbb071599d3d1d050d3509299c67f2e11
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-30 13:38:50 +0700

    update 2

---
+++ src/ss3_static/exercise2/Main.java
@@ -0,0 +1,18 @@
+package ss3_static.exercise2;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        double a, b;
+        System.out.print("Nhap a: ");
+        a = sc.nextDouble();
+        System.out.print("Nhap b: ");
+        b = sc.nextDouble();
+        System.out.printf("%.1f + %.1f = %.1f\n", a, b, MathUtil.add(a, b));
+        System.out.printf("%.1f - %.1f = %.1f\n", a, b, MathUtil.sub(a, b));
+        System.out.printf("%.1f x %.1f = %.1f\n", a, b, MathUtil.mul(a, b));
+        System.out.printf("%.1f / %.1f = %.1f\n", a, b, MathUtil.div(a, b));
+    }
+}
--- src/ss3_static/exercise2/MathUtil.java
+++ src/ss3_static/exercise2/MathUtil.java
@@ -1,16 +1,19 @@
 package ss3_static.exercise2;
 
 public class MathUtil {
-    static int add(int a, int b) {
+    static public double add(double a, double b) {
         return a + b;
     }
-    static int sub(int a, int b) {
+
+    static public double sub(double a, double b) {
         return a - b;
     }
-    static int mul(int a, int b) {
+
+    static public double mul(double a, double b) {
         return a * b;
     }
-    static int div(int a, int b) {
+
+    static public double div(double a, double b) {
         return a / b;
     }
 }
